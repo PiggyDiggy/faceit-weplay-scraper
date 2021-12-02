f157c20ca2fc55be4f60b7963d39f77b625215b4
Dmitry Bullow <dmitry55580@gmail.com>
2021-12-02 21:35:36 +0300

    Бот булл
    Прибрался

---
+++ main.py
@@ -0,0 +1,10 @@
+from bot import MyClient
+
+
+def main():
+    client = MyClient()
+    client.run("NzAwMDAyOTkxNDY5NDk0Mjgz.XpiC6g.qH-Yc2HgTVpUT0evK-MVH_4dhGA")
+
+
+if __name__ == "__main__":
+    main()
