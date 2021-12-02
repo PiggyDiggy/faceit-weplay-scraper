8f97aa67da263a8234eaeaa6729c83fcecc8f4ce
Dmitry Bullow <dmitry55580@gmail.com>
2021-12-02 21:52:43 +0300

    Минусанул токен

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bot import MyClient
 
 def main():
     client = MyClient()
-    client.run("NzAwMDAyOTkxNDY5NDk0Mjgz.XpiC6g.qH-Yc2HgTVpUT0evK-MVH_4dhGA")
+    client.run("TOKEN")
 
 
 if __name__ == "__main__":
